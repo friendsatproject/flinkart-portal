e719a6b3b1bf614ea7bd2aed8ff417a3cb702978
SAWANT <SAWANT@DESKTOP-3EGN4L3>
2021-07-13 20:32:54 +0530

    first commit

---
+++ flinkart-prtal/src/main/java/com/java/flinkart/FlinkartPrtalApplication.java
@@ -0,0 +1,13 @@
+package com.java.flinkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkartPrtalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkartPrtalApplication.class, args);
+	}
+
+}
---
+++ flinkart-prtal/src/test/java/com/java/flinkart/FlinkartPrtalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.flinkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkartPrtalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
